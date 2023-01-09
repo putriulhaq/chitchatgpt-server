3a2c174b1de4a12f4dc20ff3985648ca2e9ad753
putriulhaq <putriulhaq0609@gmail.com>
2023-01-09 20:52:01 +0700

    no cors

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const openai = new OpenAIApi(configuration);
 //   prompt: "what is your name?",
 // }).then((res) => {console.log(res.data.choices[0].text)});;
 app.use(bodyParser.json())
-app.use(cors({
-    origin:'*'
-}))
+// app.use(cors({
+//     origin:'*'
+// }))
 
 // app.use((req, res, next)=>{
 //     res.header("Access-Control-Allow-Origin", "")
