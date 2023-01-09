e3363e9a6d423e6cc08336d8df7508625d711994
putriulhaq <putriulhaq0609@gmail.com>
2023-01-09 20:45:22 +0700

    add some code for cors

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ const openai = new OpenAIApi(configuration);
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, secret"
+    )
+    
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+        return res.status(200).json({})
+    }
+    
+    if (req.method === 'POST' || req.method === 'PUT') {
+        req.body = escapeObject(req.body)
+    }
+    
+    next()
+})
+
 app.get("/", async (Req, res) => {
     res.send("haiiii")
 })
