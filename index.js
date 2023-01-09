a4057c9905087d06b35bb116d1b56ab9247ec5d9
putriulhaq <putriulhaq0609@gmail.com>
2023-01-09 21:41:18 +0700

    back

--- index.js
+++ index.js
@@ -22,15 +22,6 @@ const openai = new OpenAIApi(configuration);
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-});
 
 app.get("/", async (Req, res) => {
   res.send("haiiii");
@@ -76,4 +67,3 @@ app.listen(port, () => {
   console.log(`listening port ${port}`);
 });
 
-// console.log(response.data);
